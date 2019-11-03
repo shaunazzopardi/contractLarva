621dc181d9f9575e841bb31c7a3c101bedefaa53
shaunazzopardi <shaun.azzopardi@gmail.com>
2019-11-03 17:33:45 +0100

    exporting all functions in Parseable

--- src/Parseable.hs
+++ src/Parseable.hs
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-module Parseable (Parseable (..), _display, lineDisplay) where
+module Parseable where
 
 import Data.Char
 import Text.Parsec
